1cae6e6234c7d38e13f6493bd18e77652312496e
federer250 <80335509+federer250@users.noreply.github.com>
2022-10-15 10:55:57 -0300

    cambios en el skill2

--- src/main/java/com/portfolio/fdm/Controller/CEducacion.java
+++ src/main/java/com/portfolio/fdm/Controller/CEducacion.java
@@ -25,8 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("educacion")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://portfoliofedemelchiori.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliofedemelchiori.web.app")
 public class CEducacion {
     @Autowired
      SEducacion sEducacion;
--- src/main/java/com/portfolio/fdm/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/fdm/Security/Controller/AuthController.java
@@ -32,8 +32,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://portfoliofedemelchiori.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliofedemelchiori.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
